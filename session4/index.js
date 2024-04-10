0d892d4accb0df2ab818ab292ec456a6e8dc732f
lan anh234lan anh 234 <nguyenthilananh21092005@gmail.com>
2024-04-10 23:07:46 +0700

    add

---
+++ session4/index.js
@@ -0,0 +1,4 @@
+import {a,b,useName} from "./test.js"
+console.log("module", a,b,useName);
+import test from "./test.js"
+console.log(test);
---
+++ session4/test.js
@@ -0,0 +1,9 @@
+let a = 5;
+let b = 6;
+let useName = "bích";
+export {a,b,useName}
+
+function sayHello() {
+    console.log("hello ptit");
+}
+export default sayHello;
