6bb1d4acaa39507955724c84b37d291cd576d85e
lan anh234lan anh 234 <nguyenthilananh21092005@gmail.com>
2024-04-12 00:33:11 +0700

    add

--- session6/b3/b3.js
+++ session6/b3/b3.js
@@ -2,5 +2,11 @@
 let a1 = 9;
 let a2 = 3;
 let results;
-results = (a1 - a2);
+results = (a1 + a2);
 console.log("Kết quả phép cộng là :", results);
+results = (a1 - a2);
+console.log("Kết quả phép trừ là :", results);
+results = (a1 * a2);
+console.log("Kết quả phép nhân là :", results);
+results = (a1 / a2);
+console.log("Kết quả phép chia là :", results);
--- session6/b3/b3.ts
+++ session6/b3/b3.ts
@@ -1,7 +1,18 @@
 let a1:number = 9;
 let a2:number = 3;
 let results;
-results = (a1 - a2);
+results = (a1 + a2);
 console.log("Kết quả phép cộng là :", results);
+
+results = (a1 - a2);
+console.log("Kết quả phép trừ là :",results);
+
+results = (a1 * a2);
+console.log("Kết quả phép nhân là :",results);
+
+results = (a1 / a2);
+console.log("Kết quả phép chia là :",results);
+
+
 
 
